a69d2fedd965d554b43acb3842a4c28bbcb47cf1
smprov22 <smprovence22@gmail.com>
2019-07-19 23:17:48 -0700

    pseudocode cleaned up

--- public/app.js
+++ public/app.js
@@ -1,8 +1,7 @@
-// Grab the articles as a json
 $.getJSON("/headlines", data => {
   // For each one
   for (let i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
+
     const newDiv = $("<div>").addClass("article").append("<h2>" + data[i].title + "<br />" + "</h2>").append("<p>" + data[i].summary  + "</p>").append("<p><i>" + data[i].author + "</i></p>" + "<br>").append("<p>" + "https://www.nytimes.com" + data[i].link + "</p>" + "<hr>");
     $(newDiv).attr("data-id", data[i]._id);
 
@@ -11,71 +10,52 @@ $.getJSON("/headlines", data => {
 });
 
 
-// Whenever someone clicks a p tag
+// Click event to add a comment
 $(document).on("click", ".article", function() {
-  // Empty the notes from the note section
   $("#comments").empty();
-  // Save the id from the p tag
   let thisId = $(this).attr("data-id");
 
-  // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
     url: "/headlines/" + thisId
   })
-    // With that done, add the note information to the page
     .then((data) => {
       console.log(data);
-      // The title of the article
       $("#comments").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
       $("#comments").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
       $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
       $("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment</button>");
-      // Add a delete button
       $("#comments").append("<button data-id='" + data.comment._id + "' id='deleteComment'>Delete Comment</button>");
 
-      // If there's a note in the article
+      // If there's a comment in the article
       if (data.comment) {
-        // Place the title of the note in the title input
         $("#titleinput").val(data.comment.title);
-        // Place the body of the notes in the body textarea
         $("#bodyinput").val(data.comment.body);
       }
     });
 });
 
-// When you click the savenote button
+// Click event to save a comment
 $(document).on("click", "#saveComment", function() {
-  // Grab the id associated with the article from the submit button
   let thisId = $(this).attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/headlines/" + thisId,
     data: {
-      // Value taken from title input
       title: $("#titleinput").val(),
-      // Value taken from note textarea
       body: $("#bodyinput").val()
     }
   })
-    // With that done
     .then(data => {
-      // Log the response
-      // console.log(data);
-      // Empty the notes section
       $("#comments").empty();
     });
 
-  // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
 
+// Click event to delete a comment
 $(document).on("click", "#deleteComment", function() {
   let id = $(this).attr('data-id')
   let url = '/comments/' + id
--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 const axios = require("axios");
 const cheerio = require("cheerio");
 
@@ -29,24 +26,18 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Mongo-scraper";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
-// A GET route for scraping the echoJS website
+// A GET route for scraping the NYT Movie News website
 app.get("/scrape", (req, res) => {
-  // First, we grab the body of the html with axios
   axios.get("https://www.nytimes.com/section/movies").then((response) => {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-    // console.log(response.data)
 
-    // Now, we grab every h2 within an article tag, and do the following:
     $("li.css-ye6x8s").each((i, element) => {
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children().find("h2").text();
       result.summary = $(this)
@@ -56,15 +47,15 @@ app.get("/scrape", (req, res) => {
       result.author = $(this)
         .children().find("p.css-1xonkmu").text();
 
-      // Create a new Article using the `result` object built from scraping
+      // Create a new Headline using the `result` object built from scraping
       db.Headline.create(result)
         .then(dbHeadline => {
           // View the added result in the console
-          // console.log(dbHeadline);
+          console.log(dbHeadline);
         })
         .catch(err => {
           // If an error occurred, log it
-          // console.log(err);
+          console.log(err);
         });
     });
 
@@ -73,12 +64,10 @@ app.get("/scrape", (req, res) => {
   });
 });
 
-// Route for getting all Articles from the db
+// Route for getting all Headlines from the db
 app.get("/headlines", (req, res) => {
-  // Grab every document in the Articles collection
   db.Headline.find({})
     .then(dbHeadline => {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbHeadline);
     })
     .catch(err => {
@@ -87,14 +76,12 @@ app.get("/headlines", (req, res) => {
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
+// Route for grabbing a specific Headline by id, populate it with it's comments
 app.get("/headlines/:id", (req, res) => {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  
   db.Headline.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
     .populate("comment")
     .then(dbHeadline => {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbHeadline);
     })
     .catch(err => {
@@ -103,18 +90,14 @@ app.get("/headlines/:id", (req, res) => {
     });
 });
 
-// Route for saving/updating an Article's associated Note
+// Route for saving/updating an Headline's associated Comment
 app.post("/headlines/:id", (req, res) => {
-  // Create a new note and pass the req.body to the entry
+  // Create a new comment and pass the req.body to the entry
   db.Comment.create(req.body)
     .then(dbComment => {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Headline.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
     })
     .then(dbHeadline => {
-      // If we were able to successfully update an Article, send it back to the client
       res.json(dbHeadline);
     })
     .catch(err => {
@@ -125,11 +108,8 @@ app.post("/headlines/:id", (req, res) => {
 
 // Route for grabbing a specific Comment by id
 app.get("/comments/:id", (req, res) => {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Comment.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
     .then(dbComment => {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbComment);
     })
     .catch(err => {
