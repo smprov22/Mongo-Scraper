683bebf962ad66704040d4ce7a49a83b9479d02d
smprov22 <smprovence22@gmail.com>
2019-07-20 14:17:31 -0700

    mongo update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cheerio = require("cheerio");
 // Require all models
 const db = require("./models");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Initialize Express
 const app = express();
